3ff83a66ced79c4f50a1893c03e0766b0c66f6f0
apensotti <alexpensotti@gmail.com>
2025-10-29 01:39:06 +0000

    inital commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+export default defineNuxtConfig({
+  extends: ['docus'],
+  modules: ['@nuxtjs/plausible', '@nuxtjs/i18n'],
+  css: ['~/assets/css/main.css'],
+  site: {
+    name: 'Docus',
+  },
+  compatibilityDate: '2025-07-18',
+  i18n: {
+    defaultLocale: 'en',
+    locales: [{
+      code: 'en',
+      name: 'English',
+    }, {
+      code: 'fr',
+      name: 'Français',
+    }],
+  },
+  llms: {
+    domain: 'https://docus.dev',
+    title: 'Docus',
+    description: 'Write beautiful docs with Markdown.',
+    full: {
+      title: 'Docus',
+      description: 'Write beautiful docs with Markdown.',
+    },
+  }
+})
